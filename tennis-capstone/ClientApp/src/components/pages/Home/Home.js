02c957931605642ce99ec3ff4f5d5ca3cc75d47e
Maggie Leavell <mjleavell@gmail.com>
2019-07-23 21:21:16 -0500

    added cards

--- tennis-capstone/ClientApp/src/components/pages/Home/Home.js
+++ tennis-capstone/ClientApp/src/components/pages/Home/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
+
 import './Home.scss';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h2>Home</h2>
+        <Card>
+          <CardBody>
+            <h3 className="card-title">Tournaments</h3>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <h3 className="card-title">Rankings</h3>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            <h3 className="card-title">Favorites</h3>
+          </CardBody>
+        </Card>
       </div>
     );
   }
